6124fbb3fcf1aa56862e9d88ec1abe4be221120b
nikhil <sg804595@gmail.com>
2024-12-21 11:06:49 +0530

    added dashboard for parents admin student teacher

---
+++ src/app/(dashboard)/admin/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const AdminPage = () => {
+  return (
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      {/* LEFT */}
+      <div className='w-full lg:w-2/3'>
+
+      </div>
+
+
+      {/* Right */}
+      <div className='w-full lg:w-1/3'>
+
+      </div>
+
+
+    </div>
+  )
+}
+
+export default AdminPage
