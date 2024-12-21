9f4481b8875517cb3858727fcf4cd82bae316da4
nikhil <sg804595@gmail.com>
2024-12-22 00:26:10 +0530

    complete home page for each users

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import Announcement from "@/components/Announcement";
+import BigCalendar from "@/components/BigCalendar";
+import EventCalendar from "@/components/EventCalendar";
+import React from "react";
+import Calendar from "react-calendar";
 
 const StudentPage = () => {
   return (
-    <div className=''>
-      StudentPage
-    </div>
-  )
-}
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendar />
+        </div>
+      </div>
 
-export default StudentPage
+      {/* RIGHT */}
+
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcement />
+      </div>
+    </div>
+  );
+};
+
+export default StudentPage;
